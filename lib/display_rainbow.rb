9a8549a27b3eafa21fb956c447793c36709b57b9
Kathy Nguyen <kathyvng@gmail.com>
2018-05-16 00:44:52 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,3 +8,11 @@ def display_rainbow(colors)
   puts "#{5}"
   puts "#{6}"
 end
+colors= ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+puts "#{0}"
+puts "#{1}"
+puts "#{2}"
+puts "#{3}"
+puts "#{4}"
+puts "#{5}"
+puts "#{6}"
