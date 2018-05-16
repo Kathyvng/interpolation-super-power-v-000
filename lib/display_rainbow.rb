6a319e033793fdac7c19bb46f35b517fd3fdc731
Kathy Nguyen <kathyvng@gmail.com>
2018-05-16 01:53:43 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,3 +8,11 @@ puts "#{colors[4]}"
 puts "#{colors[5]}"
 puts "#{colors[6]}"
 end
+colors= ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+puts "#{colors[0]}"
+puts "#{colors[1]}"
+puts "#{colors[2]}"
+puts "#{colors[3]}"
+puts "#{colors[4]}"
+puts "#{colors[5]}"
+puts "#{colors[6]}"
